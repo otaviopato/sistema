8148a2ac3689573a96ea4a869764b99632131fe8
Otávio Pato <61769935+otaviopato@users.noreply.github.com>
2021-03-04 21:45:48 -0300

    Delete cadastro.sql

--- cadastro.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE aut_noticias (
-id int(11) NOT NULL auto_increment PRIMARY KEY,
-titulo varchar(255) NOT NULL default '''''''',
-conteudo text NOT NULL,
-autor_id int (11) NOT NULL default ''''0'''',
-data int (11) NOT NULL default ''''0''''
-) ENGINE=MyISAM AUTO_INCREMENT=4
